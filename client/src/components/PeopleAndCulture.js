38c7f2d37c6520dfd0be8dc6e5ff6f7639b741b4
sophie <sophieleila@icloud.com>
2020-06-08 20:02:10 +0200

    complete editor container styles

--- client/src/components/PeopleAndCulture.js
+++ client/src/components/PeopleAndCulture.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styles from "./library/editor-container/style.module.css"; // Import css modules stylesheet as styles
-import icon from "./assets/Info.png";
+import infoIcon from "./assets/Info.png";
+import noteIcon from "./assets/Note.png";
 
 class PeopleAndCulture extends Component {
   constructor(props) {
@@ -22,11 +23,29 @@ class PeopleAndCulture extends Component {
       <div className={styles.editorContainer}>
         <div className={styles.content}>
           <span className={styles.heading}>People and culture</span>
-          <div className={styles.callout}></div>
-          <div className={styles.calloutText}></div>
-          <div className={styles.info}></div>
-          <img src={icon} className={styles.info}></img>{" "}
-          <div className={styles.rectangle}></div>
+          <div className={styles.callout}>
+            <div className={styles.calloutText}>
+              Here you’ll find all the important information related to our
+              internal culture. You can read about our story, learn about our
+              values, and get the full overview of the tools we use.
+            </div>
+            <img src={infoIcon} className={styles.info}></img>{" "}
+            <div className={styles.rectangle}></div>
+          </div>
+          <div className={styles.noteLinkList}>
+            <div className={styles.ourCulture}>
+              <img src={noteIcon} className={styles.note}></img>{" "}
+              <div className={styles.text}>Our Culture</div>
+            </div>
+            <div className={styles.values}>
+              <img src={noteIcon} className={styles.note}></img>{" "}
+              <div className={styles.text}>Values</div>
+            </div>
+            <div className={styles.tipsTricks}>
+              <img src={noteIcon} className={styles.note}></img>{" "}
+              <div className={styles.text}>Tips & Tricks</div>
+            </div>
+          </div>
         </div>
       </div>
     );
