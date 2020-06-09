c84d2da4dd02490ae7b2ec85bd47ea84ae76bd52
sophie <sophieleila@icloud.com>
2020-06-10 00:30:41 +0200

    complete sidebar styles

--- client/src/components/library/sidebar/Sidebar.js
+++ client/src/components/library/sidebar/Sidebar.js
@@ -7,21 +7,12 @@ class Sidebar extends Component {
   render() {
     return (
       <div className={styles.sidebar}>
-        <div className={styles.divider}></div>
-        <div className={styles.note}>
-          <NavLink to="/">
-            ⭐ Favorites <img src={chevron} className={styles.chevron}></img>
-          </NavLink>{" "}
-        </div>
-        <div className={styles.note}>
-          <NavLink to="/people-and-culture">🥇 People and culture</NavLink>
-        </div>
-        <div className={styles.note}>
-          <NavLink to="/policies">📚 Policies</NavLink>
-        </div>
-        <div className={styles.note}>
-          <NavLink to="/management-resources">📖 Management resources</NavLink>
-        </div>
+        <NavLink to="/">
+          ⭐ Favorites <img src={chevron} className={styles.chevron}></img>
+        </NavLink>{" "}
+        <NavLink to="/people-and-culture">🥇 People and culture</NavLink>
+        <NavLink to="/policies">📚 Policies</NavLink>
+        <NavLink to="/management-resources">📖 Management resources</NavLink>
       </div>
     );
   }
