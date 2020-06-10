ed7ea3be5da7d957026a0be0263f1f616e3065d0
sophie <sophieleila@icloud.com>
2020-06-10 21:38:03 +0200

    implement workaround for default pointer styles

--- client/src/components/library/topbar/Topbar.js
+++ client/src/components/library/topbar/Topbar.js
@@ -1,17 +1,26 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import styles from "./style.module.css"; // Import css modules stylesheet as styles
-import starOnClick from "./i-star.svg";
-import star from "./i-star-outline.svg";
+import star from "./i-star.svg";
+import outline from "./i-star-outline.svg";
+import { NavLink } from "react-router-dom";
 
-class Topbar extends Component {
-  render() {
-    return (
-      <div className={styles.topbar}>
-        <span className={styles.favorites}>⭐ Favorites</span>
-        <img src={star} className={styles.star}></img>
+const Topbar = () => {
+  const [favorited, setFavorited] = useState(false);
+  return (
+    <div className={styles.topbar}>
+      <NavLink to="/" className={styles.home}>
+        ⭐ Favorites
+      </NavLink>
+
+      <div onClick={() => setFavorited(!favorited)}>
+        {favorited ? (
+          <img src={star} className={styles.star}></img>
+        ) : (
+          <img src={outline} className={styles.outline}></img>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Topbar;
